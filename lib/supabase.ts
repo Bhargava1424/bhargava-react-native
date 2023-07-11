66e43fce40fb7e26e0bfbcb337139ad6e325a7d2
Bhargava1424 <sribhargava2002@gmail.com>
2023-07-11 20:32:39 +0530

    Initial commit

---
+++ lib/supabase.ts
@@ -0,0 +1,27 @@
+import 'react-native-url-polyfill/auto'
+import * as SecureStore from 'expo-secure-store'
+import { createClient } from '@supabase/supabase-js'
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key)
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value)
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key)
+  },
+}
+
+const supabaseUrl = "https://fnzckibeqzakeientlgs.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZuemNraWJlcXpha2VpZW50bGdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODg2NjEwNjUsImV4cCI6MjAwNDIzNzA2NX0.AKsy9ioTVdmc1cDUG2r6ApaNvu-VtNZugkF_t0yV6eY"
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+})
